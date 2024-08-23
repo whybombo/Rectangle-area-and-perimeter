7cf865fc49819840197d4da3226f1eb1fabee6df
Blake Nelson <168068011+whybombo@users.noreply.github.com>
2024-08-22 23:30:48 -0500

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+width = float(input("Enter the width of the rectangle: "))
+length = float(input("Enter the length of the rectangle: "))
+
+area = width * length
+
+perimeter = (width + length) * 2
+
+print("Area of the Rectangle is %.2f" %(area))
+print("Perimeter of the Rectangle is %.2f" %(perimeter))
